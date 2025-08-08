9bff383f8dbd32d534df219ddd1cc8c76065d2ff
jonathandeng7 <jmdeng05@gmail.com>
2025-08-07 20:37:20 -0400

    project rework

---
+++ StockScreener/StockScreenerTests/StockScreenerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockScreenerTests.swift
+//  StockScreenerTests
+//
+//  Created by Jonathan on 8/7/25.
+//
+
+import Testing
+@testable import StockScreener
+
+struct StockScreenerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
